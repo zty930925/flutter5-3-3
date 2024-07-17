731ca474cace91faf3595c24e706ded4580d2774
zty930925 <a0918772036@gmail.com>
2024-07-17 14:26:43 +0800

    flutter5-3-3

---
+++ lib/screens/post_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_book_remote_example/components/post_text.dart';
+
+class PostScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PostText(),
+    );
+  }
+}
